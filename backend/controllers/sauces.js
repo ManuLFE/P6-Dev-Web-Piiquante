07f6749d3c34a960281493959582563f6f2d2d4e
ManuLFE <emmanuel.lafosse@gmail.com>
2022-09-27 22:13:53 +0200

    Renamed sauce.js to sauces.js

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -5,10 +5,8 @@ const fs = require('fs');
 exports.createSauce = (req, res, next) => {
   const sauceObject = JSON.parse(req.body.sauce);
   delete sauceObject._id;
-  delete sauceObject._userId;
   const sauce = new Sauce({
     ...sauceObject,
-    userId: req.auth.userId,
     imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
   });
   sauce.save()
